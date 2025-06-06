d5566c3541acdbdcda6de3ca5dcfea801651b4ab
wellington <wellington.h.s.oliveira@hotmail.com>
2025-06-06 11:47:05 -0300

    Project created

---
+++ Back-end/dscatalogSpring/src/main/java/com/wellington/dscatalogSpring/DscatalogSpringApplication.java
@@ -0,0 +1,13 @@
+package com.wellington.dscatalogSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogSpringApplication.class, args);
+	}
+
+}
