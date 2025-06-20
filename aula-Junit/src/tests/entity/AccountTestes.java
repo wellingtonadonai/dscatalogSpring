0d428f6ee68227d4dcb9a070e4c137a1e0ad25e9
wellington <wellington.h.s.oliveira@hotmail.com>
2025-06-20 18:27:31 -0300

    testes realizados proximo capitulo

--- aula-Junit/src/tests/entity/AccountTestes.java
+++ aula-Junit/src/tests/entity/AccountTestes.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import entity.Account;
+import tests.factory.AccountFactory;
 
 public class AccountTestes {
 	@Test
@@ -11,12 +12,68 @@ public class AccountTestes {
 		
 		double amount  = 200.0;
 		double expectedValue = 196.0;
-		Account acc = new Account(1L, 0.0);
-		
+		Account acc = AccountFactory.createEmptyAccount();
 		acc.deposit(amount);
 		
 		Assertions.assertEquals(expectedValue, acc.getBalance());
 		
 	}
+	@Test
+	public void epositShouldNotingheWhenNegativeAmount() {
+		
+		double expectedValue = 100.0;
+		Account acc = AccountFactory.createAccount(expectedValue);
+		double amount = -200.0;
+		
+		acc.deposit(amount);
+		
+		Assertions.assertEquals(expectedValue, acc.getBalance());
+	}
+	@Test
+	public void fullWithdrawShouldClearBalance() {
+		
+		double initialBalance = 800.0;
+		double expectedValue = 0.0;
+		Account acc = AccountFactory.createAccount(800.0);
+		double result = acc.fullWithDraw();
+		
+		Assertions.assertTrue(expectedValue == acc.getBalance());
+		Assertions.assertTrue(result == initialBalance);
+	}
+	@Test
+	public void withDrawShouldDecreaseBalanceWhenSufficientBalance() {
+		Account acc = AccountFactory.createAccount(800.0);
+		
+		acc.withdraw(500.0);
+		
+		Assertions.assertEquals(300.0, acc.getBalance());
+		
+		
+	}
+	@Test
+	public void withDrawShouldThrowExceptionInsufficientBalance() {
+		
+		Assertions.assertThrows(IllegalArgumentException.class, ()->{
+			
+			Account acc = AccountFactory.createAccount(800.0);
+			acc.withdraw(801.0);
+			
+		});
+		
+		
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
